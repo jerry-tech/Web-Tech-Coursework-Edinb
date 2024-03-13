2c4dbe1a8ea6669f93cd3c1b9b5ae189e8b7f323
Jeremiah Udoh <ujeremiah200@gmail.com>
2024-03-13 20:39:18 +0100

    Initial Home Page Design

---
+++ js/server.js
@@ -0,0 +1,18 @@
+function openClose() {
+  var trigger = document.getElementById("topNav");
+  var aSide = document.getElementById("sideNav");
+
+  if (trigger.className === "nav-list-items") {
+    console.log("open");
+    trigger.classList.add("responsive");
+    aSide.style.width = "clamp(15rem, 50vw, 50rem)";
+
+    document.querySelector(".closebtn").classList.add("effect");
+  } else {
+    console.log("close");
+    trigger.classList.remove("responsive");
+    aSide.style.width = "0";
+
+    document.querySelector(".closebtn").classList.remove("effect");
+  }
+}
